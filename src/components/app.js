ab4ff807c5ff2479f3c18ff815b9d55cf4ba56f8
Nick Nauert <nicknauert@gmail.com>
2017-11-28 12:34:56 -0500

    header / auth progress

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>React simple starter</div>
     );
   }
 }
-
-function mapStateToProps(state) {
-   return { posts: state.posts };
-}
-
-export default connect(mapStateToProps)(App);
