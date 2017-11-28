3e0c618b322219efcb7c664a05455f6c8f76f11d
Nick Nauert <nicknauert@gmail.com>
2017-11-28 12:55:27 -0500

    progress

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+        {this.props.children}
+      </div>
     );
   }
 }
---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Sekret Krust Krab Krabby Patty Recipe
+            <ul>
+                <li>Love</li>
+                <li>Friendship</li>
+                <li>Cookies</li>
+                <li>Sand</li> 
+            </ul>
+        </div>
+    )
+}
