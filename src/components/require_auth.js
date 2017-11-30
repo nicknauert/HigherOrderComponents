c0ad9aa1e2f99878d1ba28f77593e7e326ad4986
Nick Nauert <nicknauert@gmail.com>
2017-11-30 14:38:51 -0500

    handling context and Router

--- src/components/require_auth.js
+++ src/components/require_auth.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
     class Authentication extends Component {
+        static contextTypes = {
+            router: React.PropTypes.object
+        }
+
+        componentWillMount() {
+            this.context.router.push('/');
+        }
+
         render(){
+            console.log(this.context);
             return <ComposedComponent {...this.props} />
         }
     }
-    return Authentication;
+
+    function mapStateToProps(state){
+        return { authenticated: state.authenticated }
+    }
+
+    return connect(mapStateToProps)(Authentication);
 }
